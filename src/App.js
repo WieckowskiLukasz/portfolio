0b239e12ae759ccf96cdc67af97bfcdc3dfe40d6
Łukasz Więckowski <lukasz.wieckowski@onet.eu>
2022-06-16 13:11:52 +0200

    Added routing.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import { Route , Routes} from 'react-router';
+import { HashRouter } from 'react-router-dom';
+import Header from './layouts/Header';
+import Footer from './layouts/Footer';
+import Home from './pages/Home';
+import About from './pages/About';
+import Contact from './pages/Contact';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <HashRouter hashtype='noslash'>
+        <header>
+          <Header/>
+        </header>
+        <div className='container'>
+          <main>
+            <Routes>
+              <Route exact path='/' element={<Home/>}/>
+              <Route path='/o-mnie' element={<About/>}/>
+              <Route path='/kontakt' element={<Contact/>}/>
+              <Route path='*' element={<Home/>}/>
+            </Routes>
+          </main>
+          <footer>
+            <Footer/>
+          </footer>
+        </div>
+      </HashRouter>
     </div>
   );
 }
