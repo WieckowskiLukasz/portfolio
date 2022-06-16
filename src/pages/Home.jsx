8a990fda9b98048dccd53ba021721ead3072b86e
Łukasz Więckowski <lukasz.wieckowski@onet.eu>
2022-06-16 17:57:51 +0200

    Added content to the home page. Changed the logo. Dodano ikony Line Awesome.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,33 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 import home from '../images/backgrounds/home.jpg';
 
 export default function Home() {
   return (
     <>
-        <div className='background-image' style={{
-        backgroundImage:`url(${home})`,
-        filter: `brightness(90%)`,
-      }}></div>
+			<div className='home'>
+				<div 
+					className='background-image'
+					style={{
+						backgroundImage:`url(${home})`,
+						filter: `brightness(90%)`,
+					}}>
+				</div>
+				<div className='home-info'>
+					<div className='home-info__name'>Łukasz<br></br>Więckowski</div>
+					<div className='home-info__about-me'>
+						Jestem początkującym front-end developerem. Interesuję się tworzeniem aplikacji webowych korzystając z technologii ReactJS i SaaS.
+					</div>
+					<div className='home-info__more-info'>
+						<div className='home-info__btn-more-info'><NavLink to='/o-mnie'>Więcej o mnie<i class="las la-angle-double-right"></i></NavLink></div>
+						<div className='home-info__media-links-container'>
+							<a href='#' className='home-info__media-link'><i class="lab la-linkedin-in"></i></a>
+							<a href='#' className='home-info__media-link'><i class="lab la-github"></i></a>
+							<a href='#' className='home-info__media-link cv'>CV</a>
+						</div>
+					</div>
+				</div>
+			</div>
     </>
-  )
-}
+  );
+};
